597835e0c862c1827a54323d1f8ddde27a64065d
Mark Ellis <markellisdev@gmail.com>
2020-12-07 23:42:03 -0600

    updated routes to fix added imgs to test

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
+from flask import url_for, redirect
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return "Hello, World!"
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, Betty!</h1>
+        <img src="static/ded905929f9f27d5e6f084b799f4c8f1d145ec72.jpg" align="middle" />
+    </body>
+</html>'''
+# @app.route('/display/<filename>')
+# def display_image(filename):
+# 	#print('display_image filename: ' + filename)
+# 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+# THIS DOESNT WORK <img src="{{ url_for('static', filename='ded905929f9f27d5e6f084b799f4c8f1d145ec72.jpg') }}" align="middle" />
