99bc34f865059dc731871e2c1cdf1cdf5575e399
Mark Ellis <markellisdev@gmail.com>
2020-12-08 12:55:36 -0600

    updated paths for imgs

--- display.py
+++ display.py
@@ -5,6 +5,6 @@ import os
 @app.route('/')
 @app.route('/index', strict_slashes=False)
 def index():
-    image_names = os.listdir('./imageStore')
+    image_names = os.listdir('app/static/')
     print(image_names)
-    return render_template("display.html")
+    return render_template("display.html", len = len(image_names), image_names = image_names)
