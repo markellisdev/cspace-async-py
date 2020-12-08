bb13a121d05d9f8655d817f6e66a0fab007e02c9
Mark Ellis <markellisdev@gmail.com>
2020-12-08 11:53:46 -0600

    jinja finally working after moving into templates can't call direct apparently images tested - now to scale

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,19 @@
 from app import app
-from flask import url_for, redirect
+from flask import url_for, redirect, send_from_directory, render_template
+import os
 
-@app.route('/')
-@app.route('/index/')
-def index():
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, Betty!</h1>
-        <img src="static/ded905929f9f27d5e6f084b799f4c8f1d145ec72.jpg" align="middle" />
-    </body>
-</html>'''
+
 # @app.route('/display/<filename>')
 # def display_image(filename):
 # 	#print('display_image filename: ' + filename)
 # 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 # THIS DOESNT WORK <img src="{{ url_for('static', filename='ded905929f9f27d5e6f084b799f4c8f1d145ec72.jpg') }}" align="middle" />
+# @app.route('/static/<filename>')
+# def send_image(filename):
+#     return send_from_directory("./imageStore", filename)
+
+# @app.route('/gallery', methods=['GET', 'POST'])
+# def get_gallery():
+#     image_names = os.listdir('./imageStore')
+#     print(image_names)
+#     return render_template("gallery.html", image_names=image_names)
--- display.py
+++ display.py
@@ -1,1 +1,10 @@
 from app import app
+from flask import url_for, redirect, send_from_directory, render_template
+import os
+
+@app.route('/')
+@app.route('/index', strict_slashes=False)
+def index():
+    image_names = os.listdir('./imageStore')
+    print(image_names)
+    return render_template("display.html")
